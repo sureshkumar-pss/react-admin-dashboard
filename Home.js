2aa10a2268dcedbae94da0fecd6dd888d74d8326
surez1610 <101376816+surez1610@users.noreply.github.com>
2023-01-20 16:13:12 +0530

    1 file modified

--- Home.js
+++ Home.js
@@ -1,9 +1,27 @@
 import * as React from 'react';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 const Home =()=>{
 return (
   <div>
-    nbckdbcsfb
-  </div>
+    <Box>
+ <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        <Grid item xs={3} sx={{backgroundColor:"#321fdb"}}>
+       1
+        </Grid>
+        <Grid item xs={3} sx={{backgroundColor:"#39f"}}>
+         2
+        </Grid>
+        <Grid item xs={3}  sx={{backgroundColor:"#ffc107"}}>
+         3
+        </Grid>
+        <Grid item xs={3}  sx={{backgroundColor:"#dc3545"}}>
+          3
+        </Grid>
+      </Grid>
+      </Box>
+</div>
 )
 }
 export default Home

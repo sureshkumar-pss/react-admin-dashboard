e439a09dca60520f5b4126dd4abf85762c1b9b17
surez1610 <101376816+surez1610@users.noreply.github.com>
2023-01-24 11:37:26 +0530

    2 files modified

--- Home.js
+++ Home.js
@@ -4,6 +4,7 @@ import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import LineChart from './Chart.js'
+import Divider from '@mui/material/Divider';
 const Home =()=>{
 return (
   <div>
@@ -48,6 +49,23 @@ return (
         </Grid>
       </Grid>
       </Box>
+
+<Box sx={{boxShadow: "0px 2px 4px -1px rgb(0 0 0 / 20%) ,0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%)",padding:"10px",margin:"20px"}}>
+<Typography >Traffic & Sales </Typography>
+<Divider />
+<Grid container>
+<Grid xs={6}>
+  <Grid container>
+
+       effsdftestt
+  
+</Grid>
+<Grid xs={6}>
+
+</Grid>
+</Grid>
+</Box>
+
 </div>
 )
 }
--- Typography.js
+++ Typography.js
@@ -1,15 +1,63 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 const Typography =()=>{
+  function createData(name, calories, fat, carbs, protein) {
+    return { name, calories };
+  }
+  const rows = [
+    createData('<h1></h1>',<h1>h1. Bootstrap heading</h1>),
+    createData('<h2></h2>',<h2>h2. Bootstrap heading</h2>),
+    createData('<h3></h3>', <h3>h3. Bootstrap heading</h3>),
+    createData('<h4></h4>', <h4>h4. Bootstrap heading</h4>),
+    createData('<h5></h5>', <h5>h5. Bootstrap heading</h5>)
+  
+  ];
 return (
   <>
- <Box >
+  
+ <Box sx={{boxShadow: "0px 2px 4px -1px rgb(0 0 0 / 20%) ,0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%)"}}>
      <Box sx={{display:"flex",justifyContent:"space-between"}}>
-       <Typography>Headings</Typography>
-       <Typography>Docs
-       </Typography>
+         <h5>Headings</h5>
+         <a>Docs</a>
      </Box>
+     <Divider />
+           Documentation and examples for Bootstrap typography, including global settings, headings, body text, lists, and more.
+     <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Headings</TableCell>
+            <TableCell >Example</TableCell>
+           
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              // sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row" sx={{color:"red"}}>
+                {row.name}
+              </TableCell>
+              <TableCell sx={{padding:"0px"}} >{row.calories}</TableCell>
+        
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+
 </Box>
 </>
 )

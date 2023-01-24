4950d855af09c7a6e2042aa504efbead4d413ffb
surez1610 <101376816+surez1610@users.noreply.github.com>
2023-01-24 17:33:31 +0530

    1 file modified

--- Typography.js
+++ Typography.js
@@ -26,7 +26,7 @@ return (
   <>
   
  <Box sx={{boxShadow: "0px 2px 4px -1px rgb(0 0 0 / 20%) ,0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%)"}}>
-     <Box sx={{display:"flex",justifyContent:"space-between"}}>
+     <Box sx={{display:"flex",justifyContent:"space-between",padding:"5px"}}>
          <h5>Headings</h5>
          <a>Docs</a>
      </Box>

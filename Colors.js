6cce2f2b46dc76d10743b10331ca8f2b753027fc
surez1610 <101376816+surez1610@users.noreply.github.com>
2023-01-24 15:14:52 +0530

    1 file modified

--- Colors.js
+++ Colors.js
@@ -3,12 +3,26 @@ import Box from '@mui/material/Box';
 const Colors =()=>{
 return (
   <>
- <Box sx={{backgroundColor:"red",height:"100%"}}>
+  <Grid container >
+<Grid xs={2}>
+<Box sx={{backgroundColor:"red",height:"100%"}}>
    red
- </Box> <br />
-  <Box sx={{backgroundColor:"blue",height:"100%"}}>
-  red
-</Box>
+ </Box>
+</Grid>
+<Grid xs={2}>
+
+</Grid>
+<Grid xs={2}>
+
+</Grid>
+<Grid xs={2}>
+
+</Grid>
+<Grid xs={2}>
+
+</Grid>
+  </Grid>
+
 </>
 )
 }
